442214090d94177371c6fb0c53881e0a44addc1f
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-01-23 11:54:25 +0300

    createdDatabaseTables

---
+++ database/migrations/2023_01_23_064630_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('posts', function (Blueprint $table) {
+        $table->id();
+        $table->integer('user_id');
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        $table->string('header');
+        $table->text('description');
+        $table->string('image');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      $table->dropForeign('posts_user_id_foreign');
+      Schema::dropIfExists('posts');
+    }
+};
