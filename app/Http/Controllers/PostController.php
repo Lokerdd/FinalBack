e25291bf15b75f28f35ce4f300acf3947bfb3cd4
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-01-23 17:58:51 +0300

    End of the day 23.01.2023

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
       return Post::with(['user:id,name,email', 'tags:name'])->get();
