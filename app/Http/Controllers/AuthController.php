29c63c38da0c082c64252b0e081bfb1d9699adda
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-01-30 16:53:53 +0300

    Some authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Validator;
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+  public function login(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'email' => ['required', 'email'],
+      'password' => ['required']
+    ]);
+    if ($validator->fails()) {
+      return response()->json($validator->getMessageBag());
+    }
+
+    if (Auth::attempt($request->only('email', 'password'))) {
+      $token = Auth::user()->createToken(config('app.name'));
+      $token->accessToken->expires_at = Carbon::now()->addDay();
+      $token->accessToken->save();
+
+      return response()->json([
+        'user' => Auth::user(),
+        'token_type' => 'Bearer',
+        'token' => $token->accessToken,
+        'expires_at' => 
+          Carbon::parse($token->accessToken->expires_at)
+            ->toDateTimeString()
+      ], Response::HTTP_OK);
+    }
+    
+    return back()->withErrors([
+      'email' => 'The provided credentials do not match our records.',
+    ]);
+  }
+
+  public function register(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'name' => ['required', 'min:4'],
+      'email' => ['required', 'email', 'unique:users'],
+      'password' => ['required', 'min:8']
+    ]);
+    if ($validator->fails()) {
+      return response()->json($validator->getMessageBag(), Response::HTTP_BAD_REQUEST);
+    }
+
+    $user = User::create(array_merge(
+      $request->only('name', 'email'),
+      ['password' => bcrypt($request->password)]
+    ));
+    $user->save();
+
+    Auth::attempt($request->only('email', 'password'));
+    $token = Auth::user()->createToken(config('app.name'));
+    $token->accessToken->expires_at = Carbon::now()->addDay();
+    $token->accessToken->save();
+
+    return response()->json([
+      'user' => Auth::user(),
+      'token_type' => 'Bearer',
+      'token' => $token->accessToken,
+      'expires_at' => Carbon::parse($token->accessToken->expires_at)->toDateTimeString()
+    ], Response::HTTP_OK);
+  }
+
+  function logout(Request $request) {
+    Auth::logout();
+
+    return response()->json([
+      'message' => 'Successfully logged out'
+    ], Response::HTTP_OK);
+  }
+}
