ffd1d935c0efdf3245102e0432ca8d44a6a044b9
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-02-13 10:10:03 +0300

    Fixed review mistakes

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('register', 'register');
     Route::middleware(['auth:api'])->group(function () {
       Route::post('logout', 'logout');
-      Route::get('getUser', 'getUserByToken');
+      Route::get('whoiam', 'getUserByToken');
     });
 });
 
