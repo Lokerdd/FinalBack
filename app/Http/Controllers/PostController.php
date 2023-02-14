786ae9c37cdc25b64b5f2a5279e545f7409f2d80
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-02-14 17:19:18 +0300

    Added tags to post news method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -10,29 +11,42 @@ use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    public function index() {
-      return Post::with(['user:id,name,email', 'tags:name'])
-        ->orderBy('id', 'desc')
-        ->get();
+  public function index() {
+    return Post::with(['user:id,name,email', 'tags:name'])
+      ->orderBy('id', 'desc')
+      ->get();
+  }
+
+  public function store(Request $request) {
+    $validated = Validator::make($request->all(), [
+      "header" => 'required|string|max:255',
+      "description" => 'required|string',
+      "tags" => 'array'
+    ]);
+    if ($validated->fails()) {
+      return response("{ 'error': 'Not valid data' }", Response::HTTP_BAD_REQUEST);
     }
 
-    public function store(Request $request) {
-      $validated = Validator::make($request->all(), [
-        "header" => 'required|string|max:255',
-        "description" => 'required|string'
-      ]);
-      if ($validated->fails()) {
-        return response("{ 'error': 'Not valid data' }", Response::HTTP_BAD_REQUEST);
+    $post = new Post;
+
+    $post->user_id = Auth::user()->id;
+    $post->header = $request->input('header');
+    $post->description = $request->input('description');
+
+    $post->save();
+
+    $tags = [];
+    foreach ($request->input('tags') as $key) {
+      if (!($tag = Tag::where('name', $key)->first())) {
+        $tag = new Tag();
+        $tag->name = $key;
       }
 
-      $post = new Post;
-
-      $post->user_id = Auth::user()->id;
-      $post->header = $request->input('header');
-      $post->description = $request->input('description');
-
-      $post->save();
-
-      return response()->json($post, Response::HTTP_OK);
+      $tags[] = $tag;
     }
+    $post->tags()->saveMany($tags);
+    $post->tags = $post->tags;
+
+    return response()->json($post, Response::HTTP_OK);
+  }
 }
