ebc85806435f0cc389cc5f65feb93270ae65d38a
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-02-14 14:50:20 +0300

    Added post method for user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-      return Post::with(['user:id,name,email', 'tags:name'])->orderBy('id', 'desc')->get();
+    public function index() {
+      return Post::with(['user:id,name,email', 'tags:name'])
+        ->orderBy('id', 'desc')
+        ->get();
+    }
+
+    public function store(Request $request) {
+      $validated = Validator::make($request->all(), [
+        "header" => 'required|string|max:255',
+        "description" => 'required|string'
+      ]);
+      if ($validated->fails()) {
+        return response("{ 'error': 'Not valid data' }", Response::HTTP_BAD_REQUEST);
+      }
+
+      $post = new Post;
+
+      $post->user_id = Auth::user()->id;
+      $post->header = $request->input('header');
+      $post->description = $request->input('description');
+
+      $post->save();
+
+      return response()->json($post, Response::HTTP_OK);
     }
 }
